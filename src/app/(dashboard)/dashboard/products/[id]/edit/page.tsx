e34f29079c485635a8270106a3be46a31b600da6
DevMak <sarunimark06@gmail.com>
2026-03-19 14:55:39 +0300

    Add Promise type to the id param

--- src/app/(dashboard)/dashboard/products/[id]/edit/page.tsx
+++ src/app/(dashboard)/dashboard/products/[id]/edit/page.tsx
@@ -6,9 +6,9 @@ import { notFound } from "next/navigation";
 
 
 type EditProductPageProps = {
-  params: {
+  params: Promise<{
     id: string;
-  };
+  }>;
 };
 
 export default async function EditProductPage({
